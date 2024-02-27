ad4881071bbdf537bf9ad12606b663307086b108
MellowMokoen <mamello3632@gmail.com>
2024-02-27 08:00:25 +0200

    Styled the 1st section using tailwind css

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -4,16 +4,24 @@ export default function Blog() {
   return (
     <div>
       <div className="text-gray">Blog</div>
-      <h1 className="text-3xl font-bold">Best of the week</h1>
-      <ul>
-        <li>Trending</li>
-        <li>Popular</li>
-        <li>Latest</li>
+      <h1 className="text-7xl py-20 font-bold">
+        Best of <span class="block">the week</span>
+      </h1>
+      <ul className="flex justify-start">
+        <li className="px-8 no-underline hover:underline cursor-pointer">
+          Trending
+        </li>
+        <li className="px-8 no-underline hover:underline cursor-pointer">
+          Popular
+        </li>
+        <li className="px-8 no-underline hover:underline cursor-pointer">
+          Latest
+        </li>
       </ul>
 
       <input
         type="search"
-        className="rounded-full outline outline-2 outline-offset-2"
+        className="rounded-full p-3 my-16 outline outline-2 outline-offset-2"
         placeholder="Read about..."
       />
     </div>
