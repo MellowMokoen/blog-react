7407b5ad9622e0d8cc2fb1bac1af0f93a103cac1
MellowMokoen <mamello3632@gmail.com>
2024-02-27 09:41:53 +0200

    Imported the images

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,17 @@
 import React from "react";
+import food1 from "../Images/food1.jpg";
+import food2 from "../Images/food2.jpg";
+import food3 from "../Images/food3.jpg";
+import gym1 from "../Images/gym1.jpg";
+import gym2 from "../Images/gym2.jpg";
+import gym3 from "../Images/gym3.jpg";
+import gym4 from "../Images/gym4.jpg";
+import tech1 from "../Images/tech1.jpg";
+import tech2 from "../Images/tech2.jpg";
+import tech3 from "../Images/tech3.png";
+import travel1 from "../Images/travel1.jpg";
+import travel2 from "../Images/travel2.jpg";
+import travel3 from "../Images/travel3.jpg";
 
 export default function Blog() {
   return (
@@ -34,7 +47,7 @@ export default function Blog() {
               This is going to be a paragraph snipet
             </div>
             <button>Read more</button>
-            <img src="" alt="story" />
+            <img src={tech1} alt="story" />
           </div>
           <div className="py-5">
             <div className="font-bold">Title</div>
@@ -42,7 +55,7 @@ export default function Blog() {
               This is going to be a paragraph snipet
             </div>
             <button>Read more</button>
-            <img src="" alt="story" />
+            <img src={food1} alt="story" className="size-32" />
           </div>
           <div className="py-5">
             <div className="font-bold">Title</div>
@@ -50,7 +63,7 @@ export default function Blog() {
               This is going to be a paragraph snipet
             </div>
             <button>Read more</button>
-            <img src="" alt="story" />
+            <img src={travel1} alt="story" />
           </div>
           <div className="py-5">
             <div className="font-bold">Title</div>
@@ -58,7 +71,7 @@ export default function Blog() {
               This is going to be a paragraph snipet
             </div>
             <button>Read more</button>
-            <img src="" alt="story" />
+            <img src={gym1} alt="story" />
           </div>
         </div>
       </div>
