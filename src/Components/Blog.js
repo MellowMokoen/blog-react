7a7c189c14d0c0220a1297d86fb72bab37293ea2
MellowMokoen <mamello3632@gmail.com>
2024-02-29 22:47:25 +0200

    Added the useState for the content filtering

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -16,14 +16,17 @@ import travel3 from "../Images/travel3.jpg";
 export default function Blog() {
   const [showThirdColumn, setShowThirdColumn] = useState(false);
   const [moreArticles, setMoreArticles] = useState(true);
+  const [contentToShow, setContentToShow] = useState(null);
 
-  const handleReadMoreClick = () => {
+  const handleReadMoreClick = (content) => {
+    setContentToShow(content);
     setShowThirdColumn(true);
   };
 
   const handleMoreArticlesClick = () => {
     setMoreArticles(true);
     setShowThirdColumn(false);
+    setContentToShow(null);
   };
 
   return (
@@ -64,7 +67,7 @@ export default function Blog() {
                 </div>
                 <button
                   className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleReadMoreClick}
+                  onClick={() => handleReadMoreClick("tech")}
                 >
                   Read more
                 </button>
@@ -74,6 +77,7 @@ export default function Blog() {
                   className="size-40 stretched-image"
                 />
               </div>
+
               <div className="py-10">
                 <div className="font-bold text-x">Benefits of healthy food</div>
                 <div className="text-sm">
@@ -81,7 +85,7 @@ export default function Blog() {
                 </div>
                 <button
                   className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleReadMoreClick}
+                  onClick={() => handleReadMoreClick("food")}
                 >
                   Read more
                 </button>
@@ -91,6 +95,7 @@ export default function Blog() {
                   className="size-40 stretched-image"
                 />
               </div>
+
               <div className="py-10">
                 <div className="font-bold text-x">
                   Roaming Renegades: Tales of Adventure, Culture, and Discovery
@@ -100,7 +105,7 @@ export default function Blog() {
                 </div>
                 <button
                   className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleReadMoreClick}
+                  onClick={() => handleReadMoreClick("travel")}
                 >
                   Read more
                 </button>
@@ -110,6 +115,7 @@ export default function Blog() {
                   className="size-40 stretched-image"
                 />
               </div>
+
               <div className="py-5">
                 <div className="font-bold text-x">
                   Sweat & Success: Empowering Your Journey to Fitness
@@ -119,7 +125,7 @@ export default function Blog() {
                 </div>
                 <button
                   className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleReadMoreClick}
+                  onClick={() => handleReadMoreClick("travel")}
                 >
                   Read more
                 </button>
@@ -137,146 +143,154 @@ export default function Blog() {
         <div className="third-column transition delay-300 duration-300 ease-in-out bg-gradient-to-t font-rubik from-red-300 to-lime-200 rounded-lg border ps-8 pr-4 shadow-inner shadow-black overflow-y-auto no-scrollbar w-1/4">
           <div className="basis-1/4">
             <div class="flex flex-col ...">
-              <div className="py-10">
-                <img
-                  src={tech2}
-                  alt="story"
-                  className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="font-bold text-x py-5">
-                  AI Insights: Navigating the Future of Technology
+              {contentToShow === "tech" && (
+                <div className="py-10">
+                  <img
+                    src={tech2}
+                    alt="story"
+                    className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="font-bold text-x py-5">
+                    AI Insights: Navigating the Future of Technology
+                  </div>
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <img
+                    src={tech3}
+                    alt="story"
+                    className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <button
+                    className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
+                    onClick={handleMoreArticlesClick}
+                  >
+                    More Articles
+                  </button>
                 </div>
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
+              )}
+              {contentToShow === "food" && (
+                <div className="py-10">
+                  <img
+                    src={food2}
+                    alt="story"
+                    className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="font-bold text-x py-5">
+                    AI Insights: Navigating the Future of Technology
+                  </div>
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <img
+                    src={food3}
+                    alt="story"
+                    className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <button
+                    className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
+                    onClick={handleMoreArticlesClick}
+                  >
+                    More Articles
+                  </button>
                 </div>
-                <img
-                  src={tech3}
-                  alt="story"
-                  className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
+              )}
+              {contentToShow === "travel" && (
+                <div className="py-10">
+                  <img
+                    src={travel2}
+                    alt="story"
+                    className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="font-bold text-x py-5">
+                    AI Insights: Navigating the Future of Technology
+                  </div>
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <img
+                    src={travel3}
+                    alt="story"
+                    className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <button
+                    className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
+                    onClick={handleMoreArticlesClick}
+                  >
+                    More Articles
+                  </button>
                 </div>
-                <button
-                  className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleMoreArticlesClick}
-                >
-                  More Articles
-                </button>
-              </div>
-              <div className="py-10">
-                <img
-                  src={food2}
-                  alt="story"
-                  className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="font-bold text-x py-5">
-                  AI Insights: Navigating the Future of Technology
+              )}
+              {contentToShow === "gym" && (
+                <div className="py-10">
+                  <img
+                    src={gym2}
+                    alt="story"
+                    className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="font-bold text-x py-5">
+                    AI Insights: Navigating the Future of Technology
+                  </div>
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <img
+                    src={gym3}
+                    alt="story"
+                    className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
+                  />
+                  <div className="text-sm leading-relaxed">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
+                    libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
+                    malesuada erat eu risus hendrerit, sed sagittis lectus
+                    pulvinar. Proin et mauris et velit feugiat vestibulum.
+                  </div>
+                  <button
+                    className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
+                    onClick={handleMoreArticlesClick}
+                  >
+                    More Articles
+                  </button>
                 </div>
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <img
-                  src={food3}
-                  alt="story"
-                  className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <button
-                  className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleMoreArticlesClick}
-                >
-                  More Articles
-                </button>
-              </div>
-              <div className="py-10">
-                <img
-                  src={travel2}
-                  alt="story"
-                  className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="font-bold text-x py-5">
-                  AI Insights: Navigating the Future of Technology
-                </div>
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <img
-                  src={travel3}
-                  alt="story"
-                  className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <button
-                  className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleMoreArticlesClick}
-                >
-                  More Articles
-                </button>
-              </div>
-              <div className="py-10">
-                <img
-                  src={gym2}
-                  alt="story"
-                  className="size-40 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="font-bold text-x py-5">
-                  AI Insights: Navigating the Future of Technology
-                </div>
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <img
-                  src={gym3}
-                  alt="story"
-                  className="size-40 my-7 stretched-image rounded-md shadow-md shadow-gray-500"
-                />
-                <div className="text-sm leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vestibulum euismod, mauris ut tincidunt vestibulum, ligula
-                  libero volutpat nisi, vitae gravida enim nisl id nisi. Duis
-                  malesuada erat eu risus hendrerit, sed sagittis lectus
-                  pulvinar. Proin et mauris et velit feugiat vestibulum.
-                </div>
-                <button
-                  className="rounded-r-lg bg-rose-500 shadow-lg shadow-gray p-2 text-sm text-white my-6"
-                  onClick={handleMoreArticlesClick}
-                >
-                  More Articles
-                </button>
-              </div>
+              )}
             </div>
           </div>
         </div>
