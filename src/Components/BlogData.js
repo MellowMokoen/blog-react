64f8069fc4199a30777713d866d75bccf30d4626
MellowMokoen <mamello3632@gmail.com>
2024-03-03 15:50:04 +0200

    Created component to store the posts data

---
+++ src/Components/BlogData.js
@@ -0,0 +1,65 @@
+import food1 from "../Images/food1.jpg";
+import food2 from "../Images/food2.jpg";
+import food3 from "../Images/food3.jpg";
+import gym1 from "../Images/gym1.jpg";
+import gym2 from "../Images/gym2.jpg";
+import gym3 from "../Images/gym3.jpg";
+import tech1 from "../Images/tech1.jpg";
+import tech2 from "../Images/tech2.jpg";
+import tech3 from "../Images/tech3.png";
+import travel1 from "../Images/travel1.jpg";
+import travel2 from "../Images/travel2.jpg";
+import travel3 from "../Images/travel3.jpg";
+
+const BlogData = [
+  {
+    id: 1,
+    title: "AI Insights: Navigating the Future of Technology",
+    image: tech1,
+  },
+  {
+    id: 2,
+    title: "Benefits of healthy food",
+    image: food1,
+  },
+  {
+    id: 3,
+    title: "Roaming Renegades: Tales of Adventure, Culture, and Discovery",
+    image: travel1,
+  },
+  {
+    id: 4,
+    title: "Sweat & Success: Empowering Your Journey to Fitness",
+    image: gym1,
+  },
+  {
+    id: 5,
+    title: "Benefits of healthy food",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, mauris ut tincidunt vestibulum, ligula libero volutpat nisi, vitae gravida enim nisl id nisi. Duis malesuada erat eu risus hendrerit, sed sagittis lectus pulvinar. Proin et mauris et velit feugiat vestibulum.",
+    image: tech2,
+  },
+  {
+    id: 6,
+    title: "Benefits of healthy food",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, mauris ut tincidunt vestibulum, ligula libero volutpat nisi, vitae gravida enim nisl id nisi. Duis malesuada erat eu risus hendrerit, sed sagittis lectus pulvinar. Proin et mauris et velit feugiat vestibulum.",
+    image: food2,
+  },
+  {
+    id: 7,
+    title: "Benefits of healthy food",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, mauris ut tincidunt vestibulum, ligula libero volutpat nisi, vitae gravida enim nisl id nisi. Duis malesuada erat eu risus hendrerit, sed sagittis lectus pulvinar. Proin et mauris et velit feugiat vestibulum.",
+    image: travel2,
+  },
+  {
+    id: 8,
+    title: "Benefits of healthy food",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, mauris ut tincidunt vestibulum, ligula libero volutpat nisi, vitae gravida enim nisl id nisi. Duis malesuada erat eu risus hendrerit, sed sagittis lectus pulvinar. Proin et mauris et velit feugiat vestibulum.",
+    image: gym2,
+  },
+];
+
+export default BlogData;
